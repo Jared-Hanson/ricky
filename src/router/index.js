99891eab1481b9a01b9ee364cae401e92747c51f
Jared Hanson <jrodhanson@gmail.com>
2020-10-26 16:24:54 -0600

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Browse from '../views/Browse.vue'
+import Cart from '../views/Cart.vue'
 Vue.use(VueRouter);
 
 const routes = [
@@ -10,6 +11,16 @@ const routes = [
     name: "Home",
     component: Home
   },
+  {
+  path: '/browse',
+  name: 'Browse',
+  component: Browse
+},
+{
+path: '/cart',
+name: 'Cart',
+component: Cart
+},
   {
     path: "/about",
     name: "About",
---
+++ src/views/Cart.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+
+  <ProductList :products="products" />
+</div>
+</template>
+
+<script>
+import ProductList from "../components/ProductList_Cart.vue"
+export default {
+  name: 'Cart',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    products() {
+      return this.$root.$data.cart
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
